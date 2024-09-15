c7eb5aeb9f7010a851d29b9a4ef096ea2d61ae8d
Daembius <julienmayard@me.com>
2024-09-15 21:02:08 +0700

    Exercise 3.2: Phonebook backend step 2 - added the info route

--- index.js
+++ index.js
@@ -32,6 +32,32 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${date.toString()}</p>
+        `)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
